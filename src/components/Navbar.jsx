7330cdb07c493428fdc8686f5a331eee1172dad2
Dejlof <adebisiayodeji20@gmail.com>
2025-03-25 19:12:53 +0100

    Nav-bar Created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex flex-row justify-around p-6 bg-orange-400 text-white'>
+      <div className='pt-3' >   
+        <h1 className='text-xl text-green-900 text-bold'>Oloja <span>MarketPlace</span></h1>
+      </div>
+      <div>
+        <ul className='list-none p-0 m-0 flex flex-row'>
+            <li className='p-2'><a href="">Home</a></li>
+            <li className='p-2'><a href="">Products</a></li>
+            <li className='p-2'><a href="">About</a></li>
+            <li className='p-2'><a href="">Contact Us</a></li>
+            <li className='p-2'><a href="">Become a Vendor</a></li>
+        </ul>
+      </div>
+      <div>
+      <ul className='list-none p-0 m-0 flex flex-row' >
+         <li className='p-2'><a href="# "><i class="fa fa-magnifying-glass "></i></a></li>
+         <li className='p-2'><a href="# "><i class="fa-regular fa-user"></i></a></li>
+         <li className='p-2'><a href="# "><i class="fa-solid fa-cart-shopping "></i></a></li>
+         </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
